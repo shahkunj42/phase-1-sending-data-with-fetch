dfb98f288b4a80e101e143d6d87f2da67bb4c943
Kunj <shahkunj42@gmail.com>
2022-06-10 15:55:42 -0400

    commit tree 1c71805e63569c8045fe2a1f97cca3903f70a2b1

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     }),
+// };
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function(res){
+//         return res.json();
+//     })
+//     .then(function(object){
+//         console.log(object)
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     })
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then(res => res.json())
+    .then(data => document.querySelector('body').textContent = data["id"])
+    .catch(e => document.querySelector('body').textContent = e.message)
+    }
